9a08a063fbed4f97cc95f7d23ca0563c0fa62afe
Sandeep Aggarwal <may-be-it-works@maybe-it-dont>
2014-12-13 18:23:41 +0000

    Adding lein profiles

--- clojure/install-lein.sh
+++ clojure/install-lein.sh
@@ -3,3 +3,7 @@ rm -f ~/bin/lein
 curl -o ~/bin/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein 
 
 chmod 755 ~/bin/lein
+
+mkdir -p ~/.lein
+
+ln -sf ~/.dotfiles/clojure/lein/profiles.clj ~/.lein/profiles.clj
---
+++ clojure/lein/profiles.clj
@@ -0,0 +1,3 @@
+{:user 
+	{:plugins [[lein-create-template "0.1.1"]]}
+}
