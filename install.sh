bf44c75167e0a08f876a6684e6a3933185924ead
Sandeep Aggarwal <may-be-it-works@maybe-it-dont>
2017-05-09 16:18:47 -0500

    Removed repo/rabbitmq dirs

--- install.sh
+++ install.sh
@@ -14,8 +14,5 @@ ln -f ~/.dotfiles/bash/bashrc ~/.bashrc
 rm -f ~/.gitconfig
 ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
 
-# Repo command
-sh ~/.dotfiles/repo/install.sh
-
 # Add sublime text
 ln -sf /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl ~/bin/sublime
