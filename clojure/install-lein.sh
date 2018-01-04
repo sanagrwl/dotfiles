a7103ecbe760a2979c4cc0c90cc3edfda329a0a8
Sandeep Aggarwal <1342667+sanagrwl@users.noreply.github.com>
2018-01-04 17:01:11 -0600

    removed clojure/go lang installation scripts

--- clojure/install-lein.sh
+++
@@ -1,5 +0,0 @@
-rm -f ~/bin/lein
-
-curl -o ~/bin/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-
-chmod 755 ~/bin/lein
--- install.sh
+++ install.sh
@@ -1,18 +1,9 @@
 # oh-my-zsh
 sh ~/.dotfiles/zsh/install.sh
 
-# go-lang
-#sh ~/.dotfiles/go-lang/install.sh
-
-# leiningen
-sh ~/.dotfiles/clojure/install-lein.sh
-
 # Bash
 ln -f ~/.dotfiles/bash/bashrc ~/.bashrc
 
 # Git Config
 rm -f ~/.gitconfig
 ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
-
-# Add sublime text
-ln -sf /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl ~/bin/sublime
